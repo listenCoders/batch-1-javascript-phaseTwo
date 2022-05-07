99f5d44ed20e5baca91eff5598f7cdc27ec164c6
Hasibul Islam <listencoders@gmail.com>
2022-05-07 23:21:48 +0600

    first commit

---
+++ Class 1/app.js
@@ -0,0 +1,43 @@
+    // Rammim 
+    // Moniya 
+    // Sabid 
+    // Ria
+    // Jahirul 
+
+
+
+    //  var
+    //  let
+    //  const
+
+    // 2014 no const no let
+    // var varname = "Sabid";
+
+    // 2015 ES6 ==> const / let 
+
+    // 90% const 10% let (var)
+
+    var x = "Sabid";
+
+
+    // es6
+    const y = "Ramim";
+
+
+    let z = "Rakib";
+
+    // y = "Omuk"
+
+
+    console.log(x);
+    console.log(y);
+    console.log(z);
+
+
+
+
+    // console.log("My name is " + x);
+    // console.log("loves playing cricket");
+    // console.log(x + " age is 20");
+
+    
