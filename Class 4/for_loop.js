460f10ef0fe37a079035b1b7dbb763c018c0dc33
Hasibul Islam <sabidofficial@gmail.com>
2022-05-21 22:52:59 +0600

    Class 4 For Loop

---
+++ Class 4/for_loop.js
@@ -0,0 +1,27 @@
+// While loop
+let a = 10;
+
+// while (a <= 20) {
+//   console.log(a);
+//   a++;
+// }
+
+
+console.log("For Loop");
+// For Loop
+
+for(let a = 2; a<=10; a = a + 2){
+    console.log(a);
+}
+
+
+
+
+// console.log("From array");
+// const Array = [1,2,3,4,5,6,7,8,9,10,11];
+
+// for (let a = 9; a < Array.length; a++) {
+//     const element = Array[a];
+//     console.log(element);    
+// }
+
