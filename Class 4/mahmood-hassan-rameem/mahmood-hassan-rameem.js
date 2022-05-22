faf5423f129162bb54b6493ef86e6bbf5ab7af78
rameem2003 <rameem2019@gmail.com>
2022-05-22 07:53:09 +0600

    For loop

---
+++ Class 4/mahmood-hassan-rameem/mahmood-hassan-rameem.js
@@ -0,0 +1,49 @@
+/**
+ * Print even numbers from 1 to 50
+ * Using JavaScript While 
+ */
+
+console.log("Print even numbers from 1 to 50 (While loop)");
+let x = 0;
+
+while(x <= 50){
+    console.log(x);
+    x = x + 2
+}
+
+/**
+ * Print odd numbers from 1 to 50
+ * Using Javascript While loop
+ */
+
+console.log("Print odd numbers from 1 to 50 (While loop)");
+let y = 1;
+
+while(y <= 50){
+    console.log(y);
+    y = y + 2;
+}
+
+
+
+/**
+ * Print even numbers from 1 to 100
+ * Using Javascript for loop
+ */
+console.log("Print even numbers from 1 to 100 (For loop)");
+
+for(let x = 0; x <= 100; x = x + 2 ){
+    console.log(x);
+}
+
+
+
+/**
+ * Print odd numbers from 1 to 100
+ * Using Javascript for loop
+ */
+ console.log("Print odd numbers from 1 to 100 (For loop)");
+
+ for(let x = 1; x <= 100; x = x + 2 ){
+     console.log(x);
+ }
