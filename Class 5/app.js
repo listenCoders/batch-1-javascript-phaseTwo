f09625ef10fc6bdc105609484461e36ebdb6d055
Hasibul Islam <sabidofficial@gmail.com>
2022-05-23 23:00:40 +0600

    Class 5

---
+++ Class 5/app.js
@@ -0,0 +1,19 @@
+
+
+// console.log(x);
+// x = x + 1;
+// x++
+// let z = x++;
+// console.log("No 2 ", x);
+
+let x = 2;
+console.log(x--);
+console.log(x);
+console.log(x--);
+console.log(x--);
+
+// 1
+
+// 3 * 3 *3 *3 *3  = 9
+
+// console.log(10+20);
