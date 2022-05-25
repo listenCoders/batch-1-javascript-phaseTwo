8e2418654bc213e9f3c88e08aeaefbd4dcb3dee1
Hasibul Islam <sabidofficial@gmail.com>
2022-05-25 22:59:44 +0600

    Class 6

---
+++ Class 6/app.js
@@ -0,0 +1,31 @@
+// const obj = {
+
+// };
+// const obj1 = new Object;
+// console.log(typeof(obj));
+// console.log(obj1);
+
+// const obj = {}
+// const obj2 = new Object;
+
+// console.log(typeof(obj) );
+// console.log(typeof(obj2));
+
+// const students = 6;
+// const students2 = "6";
+// console.log(typeof(students));
+// console.log(typeof(students2));
+
+
+let lc = {
+  sabid: 6,
+  className: "Object",
+  length: "1h",
+};
+
+
+// lc = {myName:"Sabid"}
+
+// console.log(lc.myName);
+// console.log(lc.length);
+
