361d9b955a691464ba690105c2dee485bf35cb95
Hasibul Islam <sabidofficial@gmail.com>
2022-06-05 11:32:16 +0600

    Class 8

---
+++ Class 8/app.js
@@ -0,0 +1,65 @@
+                // Class 8 
+                // Video link:- https://youtu.be/3fABE51Xob0
+
+
+//////////////////////////////////////////////////////////////////////
+// lot = array
+// const items = [
+//     { user: "ratul", roll: 100 },
+//     { user: "rammim", roll: 110 },
+//     { user: "ria", roll: 120 },
+// ];
+
+
+// filter
+// items > filter > item > 
+
+// const filterItem = items.filter( function(item) {
+//     return item.roll <= 100 
+// })
+
+// console.log(filterItem[0].roll);
+
+
+
+// map 
+// const items = [
+//     { user: "ratul", roll: 100 },
+//     { user: "rammim", roll: 110 },
+//     { user: "ria", roll: 120 },
+// ];
+// const mapItem = items.map(function (params) {
+//     console.log("from map", params.roll);
+//     return params.roll
+// })
+
+// console.log("Map", mapItem[1]);
+
+// const filterItem = items.filter(function (params) {
+//     console.log("from map", params.roll);
+//     return params.roll
+// })
+// console.log("filterItem", filterItem[1]);
+
+
+// const items = [
+//     { user: "ratul", roll: 100 },
+//     { user: "rammim", roll: 110 },
+//     { user: "ria", roll: 120 },
+// ];
+// const findItem = items.find(function (params) {
+//     console.log(params.user === "ria");
+//     return params.roll === 110
+// })
+// console.log(findItem);
+
+
+
+
+
+
+// 1 = object
+// const x = {
+//   name: "ratul",
+//   roll: 10,
+// };
